3b1fa40ef2fb3c7c53af5e71f1a7d9dd7c27f4f3
Ivan Melnik <53442741+derMelnik@users.noreply.github.com>
2020-10-16 21:01:40 +0200

    Update Allocate-memory-custom-amount.ps1
    Small changes, and optimization.

--- Allocate-memory-custom-amount.ps1
+++ Allocate-memory-custom-amount.ps1
@@ -1,50 +1,81 @@
 function AllocateMemory {
-    Get-PSDrive -PSProvider 'FileSystem'
+	
+	$dateTime = (Get-Date).ToString('yyyy-MM-dd-HH-mm-ss')
+	Get-PSDrive -PSProvider 'FileSystem'
     $Drives = Get-PSDrive -PSProvider 'FileSystem' | Foreach-Object {$_.Name}
     #Write-Output "`nAvailable local drives:"
     #foreach ($DriveName in $Drives) {Write-Output $DriveName}
-    $ChoosedDrive = Read-Host -Prompt "`nPlease choose your drive for test (write only letter, check 'Name' column)"
+	$ChoosedDrive = Read-Host -Prompt "`nPlease choose your drive for test (write only letter, check 'Name' column)"
+	# against Layer8
+	$ChoosedDrive = $ChoosedDrive.ToUpper() 
+	#$ChoosedDrivecolon = ($ChoosedDrive + ":")
 
-    $ChoosedDrivecolon = ($ChoosedDrive + ":")
-    if ($Drives.Contains($ChoosedDrive)) {
-	    $FreeSpace = Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='$ChoosedDrivecolon'" | Foreach-Object {$_.FreeSpace}
-	    [uint16]$FreeSpaceGiB = ([math]::Truncate($FreeSpace/1024/1024/1024))
-		# $FreeSpaceGiB.GetType().fullname
-	    Write-Output "Your Free Space: $FreeSpaceGiB GiB"
-	    [uint16]$SpaceToAllocateGiB = Read-Host -Prompt "Please, enter the number of GiB you would like to allocate (from 1 to $FreeSpaceGiB)"
+	$ChoosedDrivecolon = ( $ChoosedDrive + ":")
+
+	if ($Drives.Contains($ChoosedDrive)) {
+		$FreeSpace = Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='$ChoosedDrivecolon'" | Foreach-Object {"{0:N0}" -f ($_.FreeSpace / 1GB)}
+		$FreeSpace = $FreeSpace -as [uint16]
+
+		Write-Output "Your Free Space: $FreeSpace GB" #Get-PSDrive gives info in GB
+
+	    [uint16]$SpaceToAllocateGB = Read-Host -Prompt "Please, enter the number of GiB you would like to allocate (from 1 to $FreeSpace GB)"
 	    # Variants of function without FreeSpaceGiB variable
-	    #$SpaceToAllocateGiB = ([math]::Truncate(($FreeSpace-(70*1024*1024*1024))/1024/1024/1024))
+	    #$SpaceToAllocateGB = ([math]::Truncate(($FreeSpace-(70*1024*1024*1024))/1024/1024/1024))
 
-        if ($ChoosedDrive -eq "C") {
-            $WritePath = "$HOME\Desktop\testmemory"
-        } else {
-            $WritePath = ($ChoosedDrive + ":\testmemory")
+		if ($ChoosedDrive -eq "C") 
+		{
+            $WritePath = "$HOME\Desktop\" + $dateTime + "testmemory"
+		} 
+		else
+		{
+            $WritePath = ($ChoosedDrive + ":\" + $dateTime + "testmemory")
         }
 
-	    if (($SpaceToAllocateGiB -gt 0) -and ($SpaceToAllocateGiB -le $FreeSpaceGiB)) { 
-			Write-Output "Going to allocate: $SpaceToAllocateGiB GiB"
-		    if (Test-Path -LiteralPath $WritePath -PathType Any) { 
+		if (($SpaceToAllocateGB -gt 0) -and ($SpaceToAllocateGB -le $FreeSpace)) 
+		{
+			Write-Output "Going to allocate: $SpaceToAllocateGB GB"
+			
+			if (Test-Path -LiteralPath $WritePath -PathType Any) 
+			{ 
 			    Write-Output "Path '$WritePath' already exists" 
-		    } else { 
-		    New-Item -ItemType directory -Path $WritePath | Foreach-Object {$_.FullName}
-		    } 
-            Write-Output "`nGenerating 1 GiB of random data`n"
-		    $out = new-object byte[] (1024*1024*1024)
-		    (new-object Random).NextBytes($out)
-            Write-Output "Writing random data to multiple files in '$WritePath'`n"
-		    Measure-Command -Expression {for ($i=1; $i -le $SpaceToAllocateGiB; $i++) { 
-			    $filename = Get-Date -Format HH.mm.ss.fff
-			    [IO.File]::WriteAllBytes("$WritePath\$filename", $out) 
-				Write-Host "`rAllocated $i/$SpaceToAllocateGiB GiB" -NoNewLine
-				if ($i -eq $SpaceToAllocateGiB) {Write-Host "`rAllocated $i/$SpaceToAllocateGiB GiB"}
-		    } }
+			} 
+			else 
+			{
+		    	New-Item -ItemType directory -Path $WritePath | Foreach-Object {$_.FullName}
+			} 
+			
+			Write-Output "`nGenerating 1 GB of random data`n"
+
+		    $out = new-object byte[] (1000*1000*1000) # stay in GB metric
+			(new-object Random).NextBytes($out)
+			Write-Output "Writing random data to multiple files in '$WritePath'`n"
+			Measure-Command -Expression{
+					for ($i=1; $i -le $SpaceToAllocateGB; $i++) 
+					{ 
+						$filename = (Get-Date).ToString('yyyy-MM-dd-HH-mm-ss')
+						[IO.File]::WriteAllBytes("$WritePath\$filename", $out)
+
+						Write-Host "`rAllocated $i/$SpaceToAllocateGB GB" -NoNewline
+
+						if ($i -eq $SpaceToAllocateGB) 
+						{
+							Write-Host "`rAllocated $i/$SpaceToAllocateGB GB"
+						}
+					}
+			}
+
             Write-Output "Done. `n"
-	    } else { 
+		} 
+		else 
+		{ 
             Clear-Host
 		    Write-Output "You have entered wrong value, please check your available free space."
             AllocateMemory
 	    }
-    } else {
+	}
+
+	else
+	{
         Clear-Host
         Write-Output "You have entered wrong drive, please choose another one `n" 
         AllocateMemory
