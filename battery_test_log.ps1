798972e6b6f9f6f6b5bfa9926f278b23548214da
ZChuckMoris <18584322+ZChuckMoris@users.noreply.github.com>
2020-11-24 08:58:54 +0300

    fixed potential null exception and code beauty adjustments in battery_test_log.ps1

--- battery_test_log.ps1
+++ battery_test_log.ps1
@@ -1,25 +1,34 @@
 function WriteBatteryPercent ([int]$Append) {
 	if ($Append -ne 0) {
-		Write-Output (([string](Get-WmiObject Win32_Battery).EstimatedChargeRemaining) + ';' + (Get-Date -Format "%H:%m:s")) `
+		Write-Output (([string]::Format((Get-WmiObject Win32_Battery).EstimatedChargeRemaining)) + ';' + (Get-Date -Format "%H:%m:s")) `
 		| Tee-Object -Filepath "battery_test_log.txt" -Append;
 	} else {
-		Write-Output (([string](Get-WmiObject Win32_Battery).EstimatedChargeRemaining) + ';' + (Get-Date -Format "%H:%m:s")) `
+		Write-Output (([string]::Format((Get-WmiObject Win32_Battery).EstimatedChargeRemaining)) + ';' + (Get-Date -Format "%H:%m:s")) `
 		| Tee-Object -Filepath "battery_test_log.txt";
 	}
 }
 
 if (-not (Test-Path -LiteralPath "battery_test_log.txt" -PathType Leaf)) {
 	Write-Output "battery percentage;time" | Tee-Object -Filepath "battery_test_log.txt";
-	WriteBatteryPercent -Append 0 -Sleep 10;
+} else {
+	Write-Output "battery percentage;time";
 }
 
-
-while ($true) { `
-    if ($BatteryPercent -ne (Get-WmiObject Win32_Battery).EstimatedChargeRemaining) {
-		$BatteryPercent = (Get-WmiObject Win32_Battery).EstimatedChargeRemaining;
-		WriteBatteryPercent -Append 1;
+while ($true) {
+	$battery=(Get-WmiObject Win32_Battery);
+	if($battery -ne $null) {
+		$CurrentPercent=$battery.EstimatedChargeRemaining;
+		if( $BatteryPercent -ne $CurrentPercent) {
+			$BatteryPercent=$currentPercent;
+			WriteBatteryPercent -Append 1;
+		}
+	} else {
+		Write-Output "PowerShell function (Get-WmiObject Win32_Battery) didn't return any information about your battery.";
+		Write-Output "Maybe it's because you are trying to run this script on desktop PC.";
+		Write-Output "If not and you are trying to run this script on laptop, then, please, contact your laptop manufacturer.";
+		pause;
 	}
-	Start-Sleep -s 10;
+	Start-Sleep -Seconds 10;
 }
 
 pause;
